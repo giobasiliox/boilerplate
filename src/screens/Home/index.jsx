a6a3ffe9c5ae7deb5e2c426549ab00877d4e6fd0
Giovana Basílio <giovana.basilio@aluno.senai.br>
2024-03-13 09:07:36 -0300

    feat: stack navigation

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -5,7 +5,21 @@ import Title from '../../components/Title'
 export default function Home() {
   return (
     <View style={styles.container}>
-     <Title title={"Home"}/>
+     <Title title={"Home 🏠"}/>
+
+     <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Categori')}
+      >
+        <Title title={'Category '} />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Profile')}
+      >
+        <Title title={'Profile '} />
+      </TouchableOpacity>
     </View>
   )
 }
